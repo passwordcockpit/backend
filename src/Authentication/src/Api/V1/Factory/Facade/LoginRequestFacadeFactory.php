aed3ef27c4409e0a346501525d442c781f1e135c
Aron Castellani <aron.castellani@blackpoints.ch>
2019-01-29 14:48:13 +0100

    Update LoginRequestFacade

--- src/Authentication/src/Api/V1/Factory/Facade/LoginRequestFacadeFactory.php
+++ src/Authentication/src/Api/V1/Factory/Facade/LoginRequestFacadeFactory.php
@@ -2,16 +2,16 @@
 
 /**
  * @see https://github.com/passwordcockpit/backend for the canonical source repository
- * @copyright Copyright (c) 2018 Blackpoints AG (https://www.blackpoints.ch)
+ * @copyright Copyright (c) 2019 Blackpoints AG (https://www.blackpoints.ch)
  * @license https://github.com/passwordcockpit/backend/blob/master/LICENSE.md BSD 3-Clause License
- * @author Davide Bucher <davide.bucher@blackpoints.ch>
  */
 
 namespace Authentication\Api\V1\Factory\Facade;
 
-use Authentication\Api\V1\Facade\LoginRequestFacade;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Container\ContainerInterface;
+use Zend\I18n\Translator\Translator;
+use Authentication\Api\V1\Facade\LoginRequestFacade;
 
 class LoginRequestFacadeFactory
 {
@@ -19,12 +19,13 @@ class LoginRequestFacadeFactory
      * Invoke method
      *
      * @param ContainerInterface $container
-     * @return DossierFacade
+     * @return LoginRequestFacade
      */
     public function __invoke(ContainerInterface $container)
     {
-        $entityManager = $container->get(EntityManagerInterface::class);
-
-        return new LoginRequestFacade($entityManager);
+        return new LoginRequestFacade(
+            $container->get(Translator::class),
+            $container->get(EntityManagerInterface::class)
+        );
     }
 }
