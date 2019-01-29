202fd8d329e18f34b7d8ec0d7347c15aadf10a57
Aron Castellani <aron.castellani@blackpoints.ch>
2019-01-29 14:41:50 +0100

    Update abstract facade

--- src/App/src/Abstracts/AbstractFacade.php
+++ src/App/src/Abstracts/AbstractFacade.php
@@ -2,14 +2,14 @@
 
 /**
  * @see https://github.com/passwordcockpit/backend for the canonical source repository
- * @copyright Copyright (c) 2018 Blackpoints AG (https://www.blackpoints.ch)
+ * @copyright Copyright (c) 2019 Blackpoints AG (https://www.blackpoints.ch)
  * @license https://github.com/passwordcockpit/backend/blob/master/LICENSE.md BSD 3-Clause License
  */
 
 namespace App\Abstracts;
 
 use Doctrine\ORM\EntityManager;
-use App\Abstracts\FacadeInterface;
+use Blackpoints\Formflow\Core\App\Abstracts\FacadeInterface;
 use Zend\Hydrator\Reflection;
 use Zend\I18n\Translator\Translator;
 
@@ -42,7 +42,7 @@ abstract class AbstractFacade implements FacadeInterface
      *
      * @param Translator $translator
      * @param EntityManager $entityManager
-     * @param type $entityName
+     * @param string $entityName
      */
     public function __construct(
         Translator $translator,
@@ -58,8 +58,9 @@ abstract class AbstractFacade implements FacadeInterface
     /**
      *
      * @param type $entity
+     * @return null
      */
-    function persist($entity)
+    public function persist($entity)
     {
         $this->entityManager->persist($entity);
         $this->entityManager->flush();
@@ -68,8 +69,9 @@ abstract class AbstractFacade implements FacadeInterface
     /**
      *
      * @param type $entity
+     * @return null
      */
-    function remove($entity)
+    public function remove($entity)
     {
         $this->entityManager->remove($entity);
         $this->entityManager->flush();
@@ -79,8 +81,29 @@ abstract class AbstractFacade implements FacadeInterface
      *
      * @return type
      */
-    function getRepository()
+    public function getRepository()
     {
         return $this->entityManager->getRepository($this->entityName);
+    }
+
+    /**
+     * Return object reference
+     *
+     * @param type $id
+     * @return type
+     */
+    public function getReference($id)
+    {
+        return $this->entityManager->getReference($this->entityName, $id);
+    }
+
+    /**
+     * Return the EntityManager object
+     *
+     * @return EntityManager
+     */
+    public function getEntityManager() : EntityManager
+    {
+        return $this->entityManager;
     }
 }
