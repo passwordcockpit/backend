267bf553e21c751b581ddb226b64083b9c54f8c5
Dominic Detta <dominic.detta@blackpoints.ch>
2019-11-20 17:49:12 +0100

    Revert cli-config to retrieve configuration from doctrine autoload

--- config/cli-config.php
+++ config/cli-config.php
@@ -1,7 +1,6 @@
 <?php
 
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\DBAL\DriverManager;
 use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
 use Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper;
 use Symfony\Component\Console\Helper\HelperSet;
@@ -9,11 +8,7 @@ use Symfony\Component\Console\Helper\HelperSet;
 $container = require __DIR__ . '/container.php';
 $em = $container->get(EntityManagerInterface::class);
 
-$dbParams = include __DIR__ . '/autoload/migrations-db.php';
-
-$connection = DriverManager::getConnection($dbParams);
-
 return new HelperSet([
-    'db' => new ConnectionHelper($connection),
+    'db' => new ConnectionHelper($em->getConnection()),
     'em' => new EntityManagerHelper($em)
 ]);
