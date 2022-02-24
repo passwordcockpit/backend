8c2167fccc9958900abc4cc4abd256723aacaf47
Christian Willemse <christian.willemse@blackpoints.ch>
2022-02-24 11:12:45 +0100

    Fixed configuration for doctrine migration.

--- migrations.php
+++ migrations.php
@@ -2,9 +2,8 @@
 
 $config = include __DIR__ . '/config/config.php';
 return [
-    'name' => 'Passwordcockpit migrations',
-    'migrations_namespace' => 'App\Migrations',
-    'migrations_directory' =>
-        $config['doctrine']['migrations']['migrations_directory'],
+    'migrations_paths' => [
+        'App\Migrations' => $config['doctrine']['migrations']['migrations_directory']
+    ],
     'all_or_nothing' => true
 ];
