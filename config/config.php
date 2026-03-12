02d5e61c09eae6084ca87c00af2bc0dfd9a83530
Michelle Jakupovic <michelle.jakupovic@blackpoints.ch>
2026-03-12 14:54:03 +0100

    chore(#114): Update to php 8.4

--- config/config.php
+++ config/config.php
@@ -12,7 +12,6 @@ $cacheConfig = [
 
 $aggregator = new ConfigAggregator(
     [
-        \Mezzio\Tooling\ConfigProvider::class,
         \Laminas\Diactoros\ConfigProvider::class,
         \Laminas\HttpHandlerRunner\ConfigProvider::class,
         \Mezzio\Router\LaminasRouter\ConfigProvider::class,
